51899024a1671e74189f1d4e95c047a7eedb6ca8
ja-gaeta <ja.gaeta@hotmail.com>
2018-03-26 16:20:26 -0300

    Adicionada aula-03

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-# JavaScript Total
+# JavaScript Total #3
 
-Código usado na vídeo-aula 2 do curso JavaScript Total.
+Código usado na vídeo-aula 3 do curso JavaScript Total.
 
-* Comentários em JavaScript.
+* Conjunto de caracteres UTF-16
+* Padrão Unicode
+* Sequências de Escape
+* Literais
+* Identificadores
+
+## Links:
+
+1. Ponto G++ - http://pontogpp.com.br/
+2. GitHub - https://github.com/ja-gaeta/javascript_total
+3. Twitter - https://twitter.com/GaetaJa
+4. vscode-icons - https://github.com/vscode-icons/vscode-icons
+5. Unicode - https://pt.wikipedia.org/wiki/Unicode
+6. UTF-16 - https://pt.wikipedia.org/wiki/UTF-16
+7. Palavras reservadas - http://www.javascripter.net/faq/reserved.htm
--- app.js
+++ app.js
@@ -1,8 +1,41 @@
-// Isto é um comentário de linha simples
-var x = 123; // atribui 123 ã variável x
+// Conjunto de Caracteres UTF-16
+
+var e = "𝑒"; // 𝑒 é um caracter com 17-bit (codepoint 0x1d452)
+var p = "π"; // π é um caracter com 16-bit (codepoint 0x03x0)
+
+console.log(p.length);  // 1: π consiste de 1 elemento de 16-bit
+console.log(e.length);  // 2: 𝑒 tem dois valores de 16-bit: "\ud835\udc52"
+
+// Sequências de Escape
+
+console.log("caf\u00e9");  // saída: "café"
+
+// Literais (literals)
+
+12                   // o número doze
+1.2                  // o número 1,2 (um inteiro e dois décimos)
+"Olá mundão velho!"  // uma cadeia (string) de texto, usando aspas duplas
+'Oi'                 // outra string, usando aspas simples
+true                 // um valor booleano (verdadeiro)
+false                // o outro valor boleando (falso)
+// /javascript/g        // uma expressão regular (regular expression)
+null                 // ausência de um objeto
+
+// Identificadores (nomes)
 
 /*
-  Este já é um comentário
-  multilinhas... as duas são comentário
+    - Começar com uma letra, sublinha (_), cifrão ($)
+    - Demais podem ser letras, dígitos, sublinhas ou cifrões
+
+    Ex.:
+            i                   // uso em contadores e loops
+            minhaVariavel       // forma preferida (camel case)
+            minha_variavel      // alternativa válida, menos usada
+            v21                 // evitar abreviações
+            _simulacro          // evitar nas designações comuns
+            $str                // evitar, exceto em jQuery
+            canção              // não recomendável
+
+    - Cuidado com as palavras reservadas da linguagem. (http://www.javascripter.net/faq/reserved.htm)
+
 */
-console.log(x);
