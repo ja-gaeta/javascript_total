878c279e5731081b0e4e7e7edf788b74c58c5c54
ja-gaeta <ja.gaeta@hotmail.com>
2018-04-15 21:04:50 -0300

    Adicionada aula-05

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
-# JavaScript Total #4
+# JavaScript Total #5
 
-Código usado na vídeo-aula 4 do curso JavaScript Total.
+Código usado na vídeo-aula 5 do curso JavaScript Total.
 
-* Variáveis
-* var
-* Hoisting
+* Tipos de Dados
+  - Números
+  - Literais Inteiros
+  - Literais Ponto-Flutuante
 
 ## Links:
 
 1. Ponto G++ - http://pontogpp.com.br/
 2. GitHub - https://github.com/ja-gaeta/javascript_total
 3. Twitter - https://twitter.com/GaetaJa
+4. IEEE754 - Curso de Java (aula 9) - https://youtu.be/JB_sPJJ5wes
+5. IEEE754 - Curso de Java (aula 10) - https://youtu.be/-lr0xWHtTxw
+6. IEEE754 - Curso de Java (aula 11) - https://youtu.be/2pAdqCYiRAo
+
--- app.js
+++ app.js
@@ -1,95 +1,30 @@
-// Variáveis em JavaScript
+// Tipos de Dados
 
-var primeiraVariavel = 1;
+// NÚMEROS
 
-/* Tipos de Dados:
+/*
+    - Sem distinção entre inteiros e ponto-flutuantes
+    - Números em JS são sempre ponto-flutuantes de 64-bit (formato no padrão IEEE 754)
+    - Precisão:
+        + Ponto-flutuante: 
+            * Mínimo: ±5 × 10^−324
+            * Máximo: ±1.7976931348623157 × 10^308
+        + Inteiros:
+            * Mínimo: −9007199254740992 (−2^53)
+            * Máximo: 9007199254740992 (2^53)
 
-Null - Explicitamente definida como nenhum valor
-Undefined - Indeterminado
-Boolean - true ou false (verdadeiro ou falso)
-String - Coleção imutável com zero ou mais caracteres Unicode
-Number - Pode ser usado em operações matemáticas
-Object - Coleção ordenada de pares contendo nome/valor
+    - Algumas operações em JS usam inteiros de 32-bit (p.ex.: índices de arrays e operadores bitwise)
 */
 
-// Números
-var borboleta = 13;
-var pi = 3.14;
-var infinito = Infinity;
-var nanNaniNana = NaN;
+// Literais Inteiros
+// Base 10
+0
+1
+500000
 
-// console.log(borboleta);
-// console.log(pi);
-// console.log(infinito);
-// console.log(nanNaniNana);
+// Base 16
+0xff  // 15 * 16 + 15 = 255 (base 10)
+0xCAFEBABE // 3.133.065.982 (base 10)
 
-// console.log(typeof(borboleta));
-// console.log(typeof(pi));
-// console.log(typeof(infinito));
-// console.log(typeof(nanNaniNana));
-
-// Strings
-var blaBlaBla = 'bla';
-var vazia = '';
-var numeroString = '1';
-var aspasDuplas = "Aspas Duplas";
-
-// console.log(blaBlaBla);
-// console.log(vazia);
-// console.log(numeroString);
-// console.log(aspasDuplas);
-
-// console.log(typeof(blaBlaBla));
-// console.log(typeof(vazia));
-// console.log(typeof(numeroString));
-// console.log(typeof(aspasDuplas));
-
-// Booleans
-
-var verdadeiro = true;
-var falso = false;
-
-// console.log(verdadeiro);
-// console.log(falso);
-// console.log(typeof(verdadeiro));
-// console.log(typeof(falso));
-
-// Undefined
-
-var declaradaUndefined = undefined;
-var nascidaIndefinida;
-
-// console.log(declaradaUndefined);
-// console.log(nascidaIndefinida);
-// console.log(typeof(declaradaUndefined));
-// console.log(typeof(nascidaIndefinida));
-
-// Objects
-var umObjeto = { 
-  nome: "Mesa",
-  tamanho: 'grande'
-};
-var objetoVoador = { borboleta: 13 };
-var hoje = new Date();
-
-// console.log(umObjeto.tamanho);
-// console.log(objetoVoador);
-// console.log(hoje);
-// console.log(typeof(umObjeto));
-// console.log(typeof(objetoVoador));
-// console.log(typeof(hoje));
-
-var primeiraVariavel;
-// console.log(primeiraVariavel);
-
-// HOISTING
-
-numero = 2;
-var numero;
-
-console.log(numero);
-
-var x = y, y = 'A';
-console.log(x + y);
-
-var a = 0, b = 0;
+// Base 8 (evitar uso)
+0377  // 3 * 64 + 7 * 8 + 7 = 255 (Base 10)
