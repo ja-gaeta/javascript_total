6221114ac0660a89653bbd139fa3a26e3cbec1e3
ja-gaeta <ja.gaeta@hotmail.com>
2018-06-05 21:14:22 -0300

    Adicionada JS-Total aula 11

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-# JavaScript Total #10
+# JavaScript Total #11
 
-Código usado na vídeo-aula 10 do curso JavaScript Total.
+Código usado na vídeo-aula 11 do curso JavaScript Total.
 
-* Arrays - Segunda Parte
+* Objetos - Literais
 
 ## Links:
 
 1.  Ponto G++ - http://pontogpp.com.br/
 2.  GitHub - https://github.com/ja-gaeta/javascript_total
 3.  Twitter - https://twitter.com/GaetaJa
-4.  Array - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array
-5.  Prototype - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/prototype
+4.  Literais de Objetos - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
--- app.js
+++ app.js
@@ -1,44 +1,46 @@
-// var umArray = [];
-// console.log(umArray);
+// Objetos - literais
 
-// Inicialização com "new"
-var palpite = new Array(2, 18, 20, 53, 65);
-console.log(palpite);
+const motoca = {
+  marca: "Yamaha",
+  modelo: "MT-07",
+  ano: 2018,
+  revendas: ["Yamais", "Indiana", "Feltrin"],
+  motor: {
+    tipo: "DOHC, 4 tempos, 8 válvulas, Refrigeração líquida",
+    cilindros: 2,
+    cilindrada: 689,
+    potenciaMax: "74,8cv (9.000rpm)"
+  },
+  getPreco: function() {
+    return "R$ 32.290,00";
+  },
+  getAno: function() {
+    return this.ano;
+  }
+};
 
-// Verificar se a variável é um array
-// var valorRetornado = Array.isArray(palpite);
+let obj;
 
-// Substituir elemento do array
-// palpite[1] = 10;
+obj = motoca;
 
-// Encontrar o índice de um elemento
-// valorRetornado = palpite.indexOf(53);
+// Acessar um valor específico
+obj = motoca.marca;
+obj = motoca["marca"];
+obj = motoca.modelo;
+obj = motoca.ano;
+obj = motoca.revendas;
+obj = motoca.motor;
+obj = motoca.getPreco();
+obj = motoca.getAno();
 
-// console.log(valorRetornado);
+// console.log(obj);
 
-// MODIFICAÇÃO DE ARRAYS
+const motocas = [
+  { marca: "Yamaha", modelo: "MT-07" },
+  { marca: "Honda", modelo: "CBR-1000RR" },
+  { marca: "Harley-Davidson", modelo: "IRON-883" }
+];
 
-// Acrescentar elemento no final do array
-// palpite.push(60);
-// palpite[4] = 24;
-
-// // Acrescentar elemento no início do array
-// palpite.unshift(5);
-
-// // Retirar último elemento do array
-// palpite.pop();
-
-// // Retirar primeiro elemento do array
-// palpite.shift();
-
-// Retirar sequência de elementos do array
-// palpite.splice(1, 3);
-
-// Inverter sequência do array
-// palpite.reverse();
-
-// Concatenação de arrays
-var palpite2 = [7, 13, 50];
-var valorRetornado = palpite.concat(palpite2);
-
-console.log(valorRetornado);
+for (let i = 0; i < motocas.length; i++) {
+  console.log(motocas[i].marca);
+}
