fb88e5f1b5251a465c31e3ad4d264f9ed1777824
ja-gaeta <ja.gaeta@hotmail.com>
2018-09-29 10:53:23 -0300

    Adicionado aula-13

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
-# JavaScript Total #12
+# JavaScript Total #13
 
-Código usado na vídeo-aula 12 do curso JavaScript Total.
+Código usado na vídeo-aula 13 do curso JavaScript Total.
 
-- Objetos - Literais (conclusão)
 - Fazendo opções com IF...ELSE
+  - Comparação de valores
+  - Aninhamento de IF...ELSE
+  - Uso de operadores lógicos
+  - O operador ternário
 
 ## Links:
 
 1.  Ponto G++ - http://pontogpp.com.br/
 2.  GitHub - https://github.com/ja-gaeta/javascript_total
 3.  Twitter - https://twitter.com/GaetaJa
-4.  Literais de Objetos - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
-5.  Cláusulas if...else - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/if...else
+4.  Cláusulas if...else - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/if...else
--- app.js
+++ app.js
@@ -1,69 +1,74 @@
-// // Literais de Objetos -- Conclusão
+// IF...ELSE
 
-// const motoca = {
-//   marca: "Yamaha",
-//   modelo: "MT-07"
-// };
+// MAIOR OU MENOR
+// --------------
 
-// let obj = motoca;
+let n = 6;
 
-// motoca.marca = "Triumph";
-// motoca.modelo = "Tiger 800";
-
-// // console.log(obj);
-
-// const carro = {
-//   marca: "Toyota",
-//   modelo: "Corolla"
-// };
-
-// // motoca = carro; // ERRO -- motoca e carro sáo constantes
-
-// carro.modelo = "Etios";
-
-// console.log(carro.modelo);
-// FAZENDO OPÇÕES COM IF...ELSE
-// ============================
-
-// if() {
-//   faça uma coisa
+// if (n < 5) {
+//   console.log("MAIOR");
 // } else {
-//   faça outra coisa
+//   console.log("MENOR");
 // }
 
-const n = "6";
+// MAIOR/IGUAL OU MENOR/IGUAL
+// --------------------------
 
-// IGUALDADE
-// ---------
+// if (n <= 6) {
+//   console.log("MAIOR OU IGUAL");
+// } else {
+//   console.log("MENOR");
+// }
 
-if (n == 6) {
-  console.log("ACERTOU");
-} else {
-  console.log("ERROU");
-}
+// IF...ELSE IF...ELSE
+// -------------------
 
-// DESIGUALDADE
-// ------------
+// const sabor = "azedo";
 
-if (n != 5) {
-  console.log("ACERTOU");
-} else {
-  console.log("ERROU");
-}
+// if (sabor === "doce") {
+//   console.log("O sabor é doce");
+// } else if (sabor === "amargo") {
+//   console.log("O sabor é amargo");
+// } else {
+//   console.log("O sabor não é doce nem amargo");
+// }
 
-// IGUALDADE DE VALOR & TIPO
-// -------------------------
+// OPERADORES LÓGICOS
+// ------------------
+
+const nome = "Manoel";
+const idade = 12;
+
+// AND (&&)
+// --------
+
+// if (idade > 0 && idade < 12) {
+//   console.log(`${nome} é uma criança.`);
+// } else if (idade >= 13 && idade < 19) {
+//   console.log(`${nome} já é um adolescente.`);
+// } else {
+//   console.log(`${nome} está maduro.`);
+// }
+
+// OR (||)
+// -------
+
+// if ((idade < 18 && idade >= 16) || idade >= 70) {
+//   console.log("VOTO FACULTATIVO");
+// } else if (idade >= 18 && idade < 70) {
+//   console.log("VOTO OBRIGATÓRIO");
+// } else if (idade < 16) {
+//   console.log("NÃO PODE VOTAR");
+// }
+
+// OPERADOR TERNÁRIO
+// -----------------
+
+n = 6;
+
+console.log(n === 6 ? "ACERTOU" : "ERROU");
 
 if (n === 6) {
-  console.log("ACERTOU");
-} else {
-  console.log("ERROU");
-}
-
-// DESIGUALDADE DE VALOR & TIPO
-// ----------------------------
-
-if (n !== 6) {
   console.log("ACERTOU");
 } else {
   console.log("ERROU");
