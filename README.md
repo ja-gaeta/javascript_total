ac0157850e6e038bdf588c5ab1d182f0858e12f2
J. A. Gaeta Mendes <ja.gaeta@hotmail.com>
2018-10-11 20:03:32 -0300

    Atualizada aula-14

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # JavaScript Total #14
 
 Código usado na vídeo-aula 14 do curso JavaScript Total.
+
+- Condicional com **switch**
+
+* Verificando dia da semana com **switch**
+
+## Links:
+
+1. Ponto G++ - http://pontogpp.com.br/
+2. GitHub - https://github.com/ja-gaeta/javascript_total
+3. Twitter - https://twitter.com/GaetaJa
+4. Condicional com switch - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/switch
--- app.js
+++ app.js
@@ -1,75 +1,72 @@
-// IF...ELSE
+// Uso de "Array.includes" para teste de critérios múltiplos
 
-// MAIOR OU MENOR
-// --------------
-
-let n = 6;
-
-// if (n < 5) {
-//   console.log("MAIOR");
-// } else {
-//   console.log("MENOR");
+// function teste(fruta) {
+//   if (fruta === "maçã" || fruta === "morango" || fruta === "amora") {
+//     console.log("Vermelha");
+//   } else {
+//     console.log("Desconhecida");
+//   }
 // }
 
-// MAIOR/IGUAL OU MENOR/IGUAL
-// --------------------------
+function teste(fruta) {
+  const frutasVermelhas = ["maçã", "morango", "amora", "uva"];
 
-// if (n <= 6) {
-//   console.log("MAIOR OU IGUAL");
-// } else {
-//   console.log("MENOR");
-// }
-
-// IF...ELSE IF...ELSE
-// -------------------
-
-// const sabor = "azedo";
-
-// if (sabor === "doce") {
-//   console.log("O sabor é doce");
-// } else if (sabor === "amargo") {
-//   console.log("O sabor é amargo");
-// } else {
-//   console.log("O sabor não é doce nem amargo");
-// }
-
-// OPERADORES LÓGICOS
-// ------------------
-
-const nome = "Manoel";
-const idade = 12;
-
-// AND (&&)
-// --------
-
-// if (idade > 0 && idade < 12) {
-//   console.log(`${nome} é uma criança.`);
-// } else if (idade >= 13 && idade < 19) {
-//   console.log(`${nome} já é um adolescente.`);
-// } else {
-//   console.log(`${nome} está maduro.`);
-// }
-
-// OR (||)
-// -------
-
-// if ((idade < 18 && idade >= 16) || idade >= 70) {
-//   console.log("VOTO FACULTATIVO");
-// } else if (idade >= 18 && idade < 70) {
-//   console.log("VOTO OBRIGATÓRIO");
-// } else if (idade < 16) {
-//   console.log("NÃO PODE VOTAR");
-// }
-
-// OPERADOR TERNÁRIO
-// -----------------
-
-n = 6;
-
-console.log(n === 6 ? "ACERTOU" : "ERROU");
-
-if (n === 6) {
-  console.log("ACERTOU");
-} else {
-  console.log("ERROU");
+  if (frutasVermelhas.includes(fruta)) {
+    console.log("Vermelha");
+  } else {
+    console.log("Desconhecida");
+  }
 }
+
+// teste("maçã");
+// teste("morango");
+// teste("pera");
+// teste("amora");
+// teste("uva");
+
+// SWITCH...CASE
+// =============
+
+const cor = "azul";
+
+// switch (cor) {
+//   case "verde":
+//     console.log("A cor é verde.");
+//     break;
+//   case "amarelo":
+//     console.log("A cor é amarelo.");
+//     break;
+//   default:
+//     console.log("A cor  não é nem verde, nem amarelo.");
+// }
+
+// Exemplo prático do uso de "switch"
+// ----------------------------------
+
+let dia;
+
+switch (new Date().getDay()) {
+  case 0:
+    dia = "domingo";
+    break;
+  case 1:
+    dia = "segunda-feira";
+    break;
+  case 2:
+    dia = "terça-feira";
+    break;
+  case 3:
+    dia = "quarta-feira";
+    break;
+  case 4:
+    dia = "quinta-feira";
+    break;
+  case 5:
+    dia = "sexta-feira";
+    break;
+  case 6:
+    dia = "sábado";
+    break;
+}
+
+// console.log(`Hoje é ${dia}.`);
