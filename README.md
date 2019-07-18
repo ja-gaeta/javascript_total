2f0e6266a21b06a4eeb87b628a4c603c4d85847e
J. A. Gaeta Mendes <ja.gaeta@hotmail.com>
2019-07-18 16:49:30 -0300

    Atualizada aula-15

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
-# JavaScript Total #14
+# JavaScript Total #15
 
-Código usado na vídeo-aula 14 do curso JavaScript Total.
+Código usado na vídeo-aula 15 do curso JavaScript Total.
 
-- Condicional com **switch**
-
-* Verificando dia da semana com **switch**
+- O objeto **Date**
 
 ## Links:
 
 1. Ponto G++ - http://pontogpp.com.br/
 2. GitHub - https://github.com/ja-gaeta/javascript_total
 3. Twitter - https://twitter.com/GaetaJa
-4. Condicional com switch - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/switch
+4. O objeto Date - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Date
+5. A Era Unix - https://pt.wikipedia.org/wiki/Era_Unix
--- app.js
+++ app.js
@@ -1,72 +1,9 @@
-// Uso de "Array.includes" para teste de critérios múltiplos
-
-// function teste(fruta) {
-//   if (fruta === "maçã" || fruta === "morango" || fruta === "amora") {
-//     console.log("Vermelha");
-//   } else {
-//     console.log("Desconhecida");
-//   }
-// }
-
-function teste(fruta) {
-  const frutasVermelhas = ["maçã", "morango", "amora", "uva"];
-
-  if (frutasVermelhas.includes(fruta)) {
-    console.log("Vermelha");
-  } else {
-    console.log("Desconhecida");
-  }
-}
-
-// teste("maçã");
-// teste("morango");
-// teste("pera");
-// teste("amora");
-// teste("uva");
-
-// SWITCH...CASE
+// O objeto DATE
 // =============
 
-const cor = "azul";
+let exemplo;
+const hoje = new Date();
 
-// switch (cor) {
-//   case "verde":
-//     console.log("A cor é verde.");
-//     break;
-//   case "amarelo":
-//     console.log("A cor é amarelo.");
-//     break;
-//   default:
-//     console.log("A cor  não é nem verde, nem amarelo.");
-// }
+exemplo = hoje;
 
-// Exemplo prático do uso de "switch"
-// ----------------------------------
-
-let dia;
-
-switch (new Date().getDay()) {
-  case 0:
-    dia = "domingo";
-    break;
-  case 1:
-    dia = "segunda-feira";
-    break;
-  case 2:
-    dia = "terça-feira";
-    break;
-  case 3:
-    dia = "quarta-feira";
-    break;
-  case 4:
-    dia = "quinta-feira";
-    break;
-  case 5:
-    dia = "sexta-feira";
-    break;
-  case 6:
-    dia = "sábado";
-    break;
-}
-
-console.log(`Hoje é ${dia}.`);
+console.log(typeof exemplo);
