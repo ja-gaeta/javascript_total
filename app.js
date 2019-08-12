8831c3b736d01b6886ab4a595360d3e1a3c46728
J. A. Gaeta Mendes <ja.gaeta@hotmail.com>
2019-08-12 18:29:42 -0300

    Adicionada aula-15

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-// O objeto DATE
-// =============
+// FUNÇÕES - FUNDAMENTOS
 
-let exemplo;
-const hoje = new Date();
+function digaOla(nome = 'José', sobrenome = 'Mané') {
+  // console.log('Olá');
+  // if (typeof nome === 'undefined') { nome = 'José' }
+  // if (typeof sobrenome === 'undefined') { sobrenome = 'Mané' }
+  return "Olá " + nome + " " + sobrenome;
+}
 
-exemplo = hoje;
+// console.log(digaOla('João', 'Silva'));
 
-console.log(typeof exemplo);
+// RETORNO DE EXPRESSÕES
+
+function quadrado(numero) {
+  return numero * numero;
+}
+
+// console.log(quadrado(8));
+
+
+// MÉTODOS
+
+const area = {
+  quadrado: function (lado) {
+    return (lado * lado);
+  },
+  circulo: function (raio) {
+    return (3.1415 * this.quadrado(raio));
+  }
+}
+
+console.log(area.circulo(1));
