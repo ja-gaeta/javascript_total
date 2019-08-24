2a25e232a2d6d1d28e51888455cbbab765be6d3a
J. A. Gaeta Mendes <ja.gaeta@hotmail.com>
2019-08-24 17:27:33 -0300

    Adicionada aula 17

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
-# JavaScript Total #16
+# JavaScript Total #17
 
-Código usado na vídeo-aula 16 do curso JavaScript Total.
+Código usado na vídeo-aula 17 do curso JavaScript Total.
 
-- Laços e Iterações
+- O DOM - Document Object Model
+- Mensagens de alerta
 
 ## Links:
 
 1. Ponto G++ - http://pontogpp.com.br/
 2. GitHub - https://github.com/ja-gaeta/javascript_total
 3. Twitter - https://twitter.com/GaetaJa
-4. Iterações (laços de repetição) - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Lacos_e_iteracoes
+4. DOM - https://developer.mozilla.org/pt-BR/docs/DOM/Referencia_do_DOM
+5. Alert - https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+6. Prompt - https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
+7. Confirm - https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm
--- app.js
+++ app.js
@@ -1,53 +1,12 @@
-// DECLARAÇÃO "FOR"
+// MENSAGENS
 
-// for (i = 0; i <= 10; i++) {
-//   console.log(i);
-// }
+// alert("Olá mundo!");
 
-// DECLARAÇÃO "WHILE"
+// const mensagem = prompt();
+// alert(mensagem);
 
-// i = 11;
-
-// while (i <= 10) {
-//   console.log(i);
-//   i++;
-// }
-
-// do {
-//   console.log(i);
-//   i++;
-// } while (i <= 10)
-
-// CONTINUE & BREAK
-
-// for (let i = 0; i < 10; i++) {
-//   if (i === 5) {
-//     continue;
-//   }
-
-//   if (i === 7) {
-//     break;
-//   }
-//   console.log(i);
-// }
-
-// FOR IN...
-
-// const moto = {
-//   fabricante: 'Honda',
-//   modelo: 'SH-150i'
-// }
-
-// for (let i in moto) {
-//   // console.log(i);
-//   // console.log(moto[i]);
-//   console.log(`${i}: ${moto[i]}`);
-// }
-
-// FOR OF...
-
-const umArray = ['Honda', 'Yamaha', 'Triumph'];
-
-for (let i of umArray) {
-  console.log(i);
-}
+if (confirm('Tem certeza?')) {
+  console.log('SIM');
+} else {
+  console.log('NÃO');
+};
