d4deba568cb212bd61f85fe93111c84a2fdf0e68
J. A. Gaeta Mendes <ja.gaeta@hotmail.com>
2020-07-16 20:02:29 -0300

    Adicionada aula-30

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
-# JavaScript Total #29
+# JavaScript Total #30
 
-## Projeto Lista de Tarefas (Manipulação do DOM) - Parte 2
+## Construtor de Objetos (constructor)
 
-Um aplicativo simples de uma lista de tarefas que permite:
-
-- Adicionar uma nova tarefa à lista;
-- Marcar uma tarefa completada com risco duplo sobre o texto ou desmarcá-la;
-- Limpar a lista de tarefas;
-- Persistir a lista de tarefas e seu estado (riscada ou não) no Local Storage.
+- Introdução ao JavaScript Orientado a Objetos.
 
 ## Links:
 
@@ -15,5 +10,4 @@ Um aplicativo simples de uma lista de tarefas que permite:
 2. GitHub - https://github.com/ja-gaeta/javascript_total
 3. Twitter - https://twitter.com/GaetaJa
 4. Bootstrap - https://getbootstrap.com/
-5. Font Awesome - https://www.bootstrapcdn.com/fontawesome/
-6. JavaScript - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript
+5. JS Orientado a Objetos - https://developer.mozilla.org/pt-BR/docs/Aprender/JavaScript/Objetos/Object-oriented_JS
--- app.js
+++ app.js
@@ -1,154 +1,39 @@
-// Javascript Total - Lista de Tarefas
-// ===================================
+// O construtor de objetos --> constructor
+// =======================================
 
-// Seletores
-// ---------
+function Moto(marca, modelo, ano) {
+  // this.marca = "Triumph";
+  // this.modelo = "Tiger 800 XRX";
+  // this.ano = 2018;
+  this.marca = marca;
+  this.modelo = modelo;
+  this.ano = ano;
 
-const form = document.getElementById("tarefa-form");
-const inputTarefa = document.getElementById("tarefaInput");
-const listaTarefas = document.querySelector(".lista");
-const limparBtn = document.getElementById("limparBtn");
-
-// console.log(form);
-
-// Event Listeners
-// ---------------
-
-loadEventListeners();
-
-function loadEventListeners() {
-  // Evento de adicionar tarefas
-  form.addEventListener("submit", adicionarTarefa);
-  // Carregar lista ao carregar o DOM
-  document.addEventListener("DOMContentLoaded", carregarTarefas);
-  // Evento de limpar lista de tarefas
-  limparBtn.addEventListener("click", limparLista);
-  // Evento de riscar ou retirar risco de tarefa
-  listaTarefas.addEventListener("click", riscarTarefa);
-}
-
-// Adicionar tarefa
-// ----------------
-
-function adicionarTarefa(e) {
-  if (inputTarefa.value === "") {
-    alert("Adicione uma tarefa");
-  }
-  // Ativa botão para limpar tarefas
-  if (limparBtn.hasAttribute("disabled")) {
-    limparBtn.removeAttribute("disabled");
-  }
-  // Criação de um elemento li
-  const li = document.createElement("li");
-  li.className = "tarefa";
-  // Criação do nó de texto e adição ao elemento li
-  li.appendChild(document.createTextNode(inputTarefa.value));
-  // Criação do elemento link (tag a) para o ícone
-  const link = document.createElement("a");
-  link.className = "check-tarefa";
-  // Adição do html do ícone
-  link.innerHTML = "<i class='fa fa-check' aria-hidden='true'></i>";
-  // Adição do link ao elemento li
-  li.appendChild(link);
-  // Adição do li ao elemento ul
-  listaTarefas.appendChild(li);
-  // Armazena no LocalStorage
-  armazenaTarefa({ tarefa: inputTarefa.value, feito: false });
-  // Limpa a caixa de input da entrada
-  inputTarefa.value = "";
-
-  e.preventDefault();
-}
-
-// Armazena tarefa
-// ---------------
-
-function armazenaTarefa(tarefa) {
-  let tarefas;
-  if (localStorage.getItem("tarefas") === null) {
-    tarefas = [];
-  } else {
-    tarefas = JSON.parse(localStorage.getItem("tarefas"));
-  }
-  tarefas.push(tarefa);
-  localStorage.setItem("tarefas", JSON.stringify(tarefas));
-}
-
-// Carregar tarefas do Local Storage
-// ---------------------------------
-
-function carregarTarefas() {
-  let tarefas;
-  if (localStorage.getItem("tarefas") === null) {
-    tarefas = [];
-  } else {
-    tarefas = JSON.parse(localStorage.getItem("tarefas"));
-  }
-
-  tarefas.forEach(function (tarefa) {
-    // Criação de um elemento li
-    const li = document.createElement("li");
-    li.className = "tarefa";
-    // Criação do nó de texto e adição ao elemento li
-    li.appendChild(document.createTextNode(tarefa.tarefa));
-    if (tarefa.feito) {
-      li.classList.add("done");
+  let encapsulado = true;
+  this.moto = function () {
+    if (encapsulado) {
+      return this.marca + " " + this.modelo + " " + this.ano;
+    } else {
+      return this.modelo;
     }
-    // Criação do elemento link (tag a) para o ícone
-    const link = document.createElement("a");
-    link.className = "check-tarefa";
-    // Adição do html do ícone
-    link.innerHTML = "<i class='fa fa-check' aria-hidden='true'></i>";
-    // Adição do link ao elemento li
-    li.appendChild(link);
-    // Adição do li ao elemento ul
-    listaTarefas.appendChild(li);
-  });
-  // Ativa botão para limpar lista
-  if (limparBtn.hasAttribute("disabled")) {
-    limparBtn.removeAttribute("disabled");
-  }
+  };
 }
 
-// Limpar lista de tarefas
-// -----------------------
+// console.log(this);
 
-function limparLista() {
-  while (listaTarefas.firstChild) {
-    listaTarefas.removeChild(listaTarefas.firstChild);
-  }
-  let atributo = document.createAttribute("disabled");
-  limparBtn.setAttributeNode(atributo);
-  localStorage.clear();
-}
+// const minhaMoto = new Moto();
+// console.log(minhaMoto.modelo);
+// minhaMoto.modelo = "Tiger 800 XRXL";
+// console.log(minhaMoto.modelo);
 
-// Riscar tarefa completada (ou retirar o risco)
-// ---------------------------------------------
+// const outraMoto = new Moto();
+// console.log(outraMoto.modelo);
 
-function riscarTarefa(e) {
-  if (e.target.parentElement.classList.contains("check-tarefa")) {
-    e.target.parentElement.parentElement.classList.toggle("done");
-  }
-  atualizaLocalStorage(e.target.parentElement.parentElement);
-}
+const minhaMoto = new Moto("Triumph", "Tiger 800 XRXL", 2018);
+console.log(minhaMoto.modelo);
+console.log(minhaMoto.encapsulado);
+console.log(minhaMoto.moto());
 
-// Atualiza LocalStorage
-// ---------------------
-
-function atualizaLocalStorage(item) {
-  let tarefas;
-  if (localStorage.getItem("tarefas") === null) {
-    tarefas = [];
-  } else {
-    tarefas = JSON.parse(localStorage.getItem("tarefas"));
-  }
-
-  tarefas.forEach(function (tarefa) {
-    if (item.textContent === tarefa.tarefa) {
-      item.classList.contains("done")
-        ? (tarefa.feito = true)
-        : (tarefa.feito = false);
-    }
-  });
-  localStorage.setItem("tarefas", JSON.stringify(tarefas));
-}
+const outraMoto = new Moto("BMW", "R 1250 GS Adventure", 2020);
+console.log(outraMoto.modelo);
+console.log(outraMoto.moto());
