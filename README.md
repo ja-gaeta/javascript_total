01fb0c1b0e38fc03f699a04cc38cfc589dc30fee
J. A. Gaeta Mendes <ja.gaeta@hotmail.com>
2020-07-22 14:23:18 -0300

    Adicionada aula-31

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# JavaScript Total #30
+# JavaScript Total #31
 
-## Construtor de Objetos (constructor)
+## Object.prototype
 
-- Introdução ao JavaScript Orientado a Objetos.
+- Introdução aos protótipos (prototypes) em JavaScript
 
 ## Links:
 
@@ -11,3 +11,5 @@
 3. Twitter - https://twitter.com/GaetaJa
 4. Bootstrap - https://getbootstrap.com/
 5. JS Orientado a Objetos - https://developer.mozilla.org/pt-BR/docs/Aprender/JavaScript/Objetos/Object-oriented_JS
+6. Prototype - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/prototype
+7. Herança e cadeia de protótipos - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Inheritance_and_the_prototype_chain
--- app.js
+++ app.js
@@ -1,39 +1,37 @@
-// O construtor de objetos --> constructor
-// =======================================
+// Object.prototype
+// ================
 
 function Moto(marca, modelo, ano) {
-  // this.marca = "Triumph";
-  // this.modelo = "Tiger 800 XRX";
-  // this.ano = 2018;
   this.marca = marca;
   this.modelo = modelo;
   this.ano = ano;
 
-  let encapsulado = true;
-  this.moto = function () {
-    if (encapsulado) {
-      return this.marca + " " + this.modelo + " " + this.ano;
-    } else {
-      return this.modelo;
-    }
-  };
+  // this.moto = function () {
+  //   return this.marca + " " + this.modelo + " " + this.ano;
+  // };
 }
 
-// console.log(this);
+// Retorna info completa
+Moto.prototype.moto = function () {
+  return this.marca + " " + this.modelo + " " + this.ano;
+};
 
-// const minhaMoto = new Moto();
-// console.log(minhaMoto.modelo);
-// minhaMoto.modelo = "Tiger 800 XRXL";
-// console.log(minhaMoto.modelo);
+// Retorna apenas o modelo
+Moto.prototype.getModelo = function () {
+  return this.modelo;
+};
 
-// const outraMoto = new Moto();
-// console.log(outraMoto.modelo);
+// Ajusta uma propriedade
+Moto.prototype.setModelo = function (novoModelo) {
+  this.modelo = novoModelo;
+};
 
 const minhaMoto = new Moto("Triumph", "Tiger 800 XRXL", 2018);
-console.log(minhaMoto.modelo);
-console.log(minhaMoto.encapsulado);
+console.log(minhaMoto);
 console.log(minhaMoto.moto());
-
-const outraMoto = new Moto("BMW", "R 1250 GS Adventure", 2020);
-console.log(outraMoto.modelo);
-console.log(outraMoto.moto());
+console.log(minhaMoto.getModelo());
+minhaMoto.setModelo("Bonneville T120");
+console.log(minhaMoto.getModelo());
+console.log(minhaMoto.hasOwnProperty("marca"));
+console.log(minhaMoto.hasOwnProperty("valor"));
+console.log(minhaMoto.hasOwnProperty("getModelo"));
